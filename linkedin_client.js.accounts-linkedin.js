340eb948864da886d40152e2e106fe38a00d11c9
Mike Asem <d.mike.asem@gmail.com>
2013-11-23 14:05:48 -0800

    new styling

--- linkedin_client.js.accounts-linkedin.js
+++
@@ -1,9 +0,0 @@
-Meteor.loginWithLinkedin = function (options, callback) {
-  var credentialRequestCompleteCallback = Accounts.oauth.credentialRequestCompleteHandler(callback);
-  LinkedIn.requestCredential(options, credentialRequestCompleteCallback);
-};
-
-Meteor.getLinkedinProfile = function (options, callback) {
-  var credentialRequestCompleteCallback = Accounts.oauth.credentialRequestCompleteHandler(callback);
-  LinkedIn.requestProfile(options, credentialRequestCompleteCallback);
-};
